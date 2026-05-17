5a274604ef5630eef202b4afb1ac93cb9483bbd7
mayanpas <mayanpas0710@gmail.com>
2026-05-17 13:46:36 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            // Quando entra na tela
+            entry.target.classList.add('show');
+        } else {
+            // Quando sai da tela (permite repetir a animação ao subir/descer)
+            entry.target.classList.remove('show');
+        }
+    });
+}, {
+    // Ajuste o threshold para 0.2 para a animação começar 
+    // apenas quando 20% do card estiver visível
+    threshold: 0.2 
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
