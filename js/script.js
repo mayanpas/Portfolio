22dd52618d4d9da04fcc81aa6223771402799c1d
mayanpas <mayanpas0710@gmail.com>
2026-05-24 14:08:03 -0300

    Versão 1.0

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver(
+  (entries) => {
     entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            // Quando entra na tela
-            entry.target.classList.add('show');
-        } else {
-            // Quando sai da tela (permite repetir a animação ao subir/descer)
-            entry.target.classList.remove('show');
-        }
+      if (entry.isIntersecting) {
+        // Quando entra na tela
+        entry.target.classList.add("show");
+      } else {
+        // Quando sai da tela (permite repetir a animação ao subir/descer)
+        entry.target.classList.remove("show");
+      }
     });
-}, {
-    // Ajuste o threshold para 0.2 para a animação começar 
+  },
+  {
+    // Ajuste o threshold para 0.2 para a animação começar
     // apenas quando 20% do card estiver visível
-    // threshold: 0.1 
-});
+    // threshold: 0.1
+  },
+);
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+// teste backgrounnd
+
 
