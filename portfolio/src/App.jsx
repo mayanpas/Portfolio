24417da339af8739af5d32be54efce05e0c10cf4
mayanpas <mayanpas0710@gmail.com>
2026-07-24 13:32:22 -0300

    teste32

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,21 +1,23 @@
 // import { useScrollAnimation } from './hooks/useScrollAnimation';
 
-import SmoothScroll from './components/SmoothScroll';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import SobreMim from './components/SobreMim';
-import Habilidades from './components/Habilidades';
-import Projetos from './components/Projetos';
-import FaleComigo from './components/FaleComigo';
-import Footer from './components/Footer';
-import FooterShadow from './components/FooterShadow';
-import './App.css'; 
+import SmoothScroll from "./components/SmoothScroll";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import SobreMim from "./components/SobreMim";
+import Habilidades from "./components/Habilidades";
+import Projetos from "./components/Projetos";
+import FaleComigo from "./components/FaleComigo";
+import Footer from "./components/Footer";
+import FooterShadow from "./components/FooterShadow";
+import Background from "./components/Background";
+import "./App.css";
 
 function App() {
   // Ativa a animação universalmente para qualquer elemento com a classe .animar
   // useScrollAnimation('.hidden');
   return (
     <SmoothScroll>
+      <Background />
       {/* <FooterShadow /> */}
       <Navbar />
       <main>
---
+++ portfolio/src/components/Background.jsx
@@ -0,0 +1,5 @@
+function Background() {
+  return <div className="background"></div>;
+}
+
+export default Background;
