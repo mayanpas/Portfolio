c55ef852e6d52cc4ce694937bc0b2b7bb85d97b0
mayanpas <mayanpas0710@gmail.com>
2026-08-01 19:42:36 -0300

    teste7

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -79,6 +79,7 @@ function App() {
             <Route path="/projeto/:id" element={<DetalhesProjeto />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </SmoothScroll>
   );
