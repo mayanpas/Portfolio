3d92416e314eae096ee9004118d59c82675c7fed
mayanpas <mayanpas0710@gmail.com>
2026-08-01 21:04:36 -0300

    teste38

--- portfolio/src/components/Contato.jsx
+++ portfolio/src/components/Contato.jsx
@@ -8,16 +8,16 @@ import {
 
 function Contato() {
   return (
-    <section id="contatoSection">
+    <section id="contatoSection" >
       <div className="sectionContent" id="contatoContent">
-        <div className="sectionText" id="contatoText">
+        <div className="sectionText" id="contatoText" data-aos="fade-up">
           <h2>Contato</h2>
           <p>
             <span>Estou disponível</span> para novas oportunidades e projetos freelances.
             Envie uma mensagem pelo e-mail ou me mande um oi nas redes sociais!
           </p>
         </div>
-        <div className="contatoButtons">
+        <div className="contatoButtons" data-aos="fade-up">
           <a
             href="mailto:mayanpas0710@gmail.com?subject=Contato do Site&body=Olá, gostaria de falar sobre..."
             className="contatoButton"
