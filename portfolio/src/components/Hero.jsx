d79dee53c46dc75aca8affc440a6fbf21f8bde46
mayanpas <mayanpas0710@gmail.com>
2026-08-01 21:06:17 -0300

    teste39

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import "../css/Hero.css";
 function Hero() {
   return (
     <section id="heroSection">
-      <div className="sectionContent" id="heroContent">
+      <div className="sectionContent" id="heroContent" data-aos="fade-up">
         <h1>
           <span id="prazer">Prazer,</span>
           <br />
