d4311daacf541b84410d3eb37d6e5a47e63d199a
mayanpas <mayanpas0710@gmail.com>
2026-08-04 19:30:30 -0300

    teste76

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -14,7 +14,8 @@ function Root() {
   useEffect(() => {
     // Inicializa o AOS
     AOS.init({
-      duration: 800, // 1000ms pode parecer um pouco lento com Smooth Scroll; 800ms costuma ser o ponto ideal
+      duration: 600, // 1000ms pode parecer um pouco lento com Smooth Scroll; 800ms costuma ser o ponto ideal
+      mirror: true, // Faz a animação rodar de forma reversa ao rolar para cima
       once: false,   // Garante que a animação aconteça apenas uma vez e não quebre durante a rolagem
       offset: 50,   // Um offset menor (50px em vez de 0/100) garante que o elemento já comece a animar assim que entra na viewport
     });
